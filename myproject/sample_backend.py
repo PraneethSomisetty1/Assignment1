bdcbf9436cf642e777f8c839ecacafd6c2e72600
PraneethSomisetty1 <psomiset@calpoly.edu>
2021-09-29 12:45:12 -0700

    added everything

--- myproject/sample_backend.py
+++ myproject/sample_backend.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -48,11 +49,15 @@ def get_users():
    if request.method == 'GET':
       search_username = request.args.get('name')
       search_job = request.args.get('job')
-      if search_username and search_job:
+      if search_username :
          subdict = {'users_list' : []}
          for user in users['users_list']:
-            if user['name'] == search_username and user['job'] == search_job:
-               subdict['users_list'].append(user)
+            if user['name'] == search_username :
+               if search_job :
+                  if user['job'] == search_job :
+                     subdict['users_list'].append(user)
+               else :
+                  subdict['users_list'].append(user)
          return subdict
       return users
    elif request.method == 'POST':
